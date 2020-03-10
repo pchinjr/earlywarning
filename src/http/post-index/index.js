324224ae8f372263cbc39b895670a7456f271892
Paul Chin Jr <pchinjr@gmail.com>
2020-03-10 08:44:02 -0400

    console log statements

--- src/http/post-index/index.js
+++ src/http/post-index/index.js
@@ -2,8 +2,10 @@ let arc = require('@architect/functions');
 let parseBody = arc.http.helpers.bodyParser;
 
 exports.handler = async function http(req) {
+  console.log(req)
 	let body = parseBody(req);
-	let text = `END ${body.text}`;
+  let text = `END ${body.text}`;
+  console.log(text)
 	return {
 		headers: {'Content-Type': 'text/plain; charset=utf8'},
 		body: text,
